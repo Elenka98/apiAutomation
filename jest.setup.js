7e93599bac1553c6206b5212a7778f872968e9dc
harma <harmashevaolena@gmail.com>
2025-05-22 15:47:00 -0400

    created testWindows and testMacOS

--- jest.setup.js
+++
@@ -1,5 +0,0 @@
-// const supertest = require('supertest')
-//
-// global.request = supertest('http://localhost:8001/api/v1')
-// global.TextEncoder = require("util").TextEncoder;
-// global.TextDecoder = require("util").TextDecoder;
